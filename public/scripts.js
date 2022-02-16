303811523d9f97a96271d77e851da2eb36ab5d35
ThinGoods <vashchenko131@gmail.com>
2022-02-16 20:55:45 +0200

    q

---
+++ public/scripts.js
@@ -0,0 +1,9 @@
+function someAction() {
+    let x = document.getElementById('inp').value 
+    if (document.getElementById('inp').value != "") {
+        document.getElementById('outp').innerHTML = x
+        document.getElementById('inp').value = ""
+    } else {
+        document.getElementById('outp').innerHTML = " "
+    }
+}
