f3baa812cc43800e3d90a192da4224d67f25274d
ThinGoods <vashchenko131@gmail.com>
2022-02-23 12:07:08 +0200

    7th task

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,23 @@
-function someFunction() {    
+function someFunction() {   
+    let divOutput = document.getElementById('outp');
+    let inputRow = document.querySelector('#inp')
+    // document.getElementById('outp').textContent = document.querySelector('#inp').value;
+    if (!inputRow.value.length) {
+        divOutput.textContent = "Пусто";
+    } else if (!isNaN(divOutput.textContent = inputRow.value)) {
+        divOutput.textContent = inputRow.value;
+    } else { divOutput.textContent = 'Введите число!' }
+}
+let inputValue = document.querySelector('#inp')
+inputValue.addEventListener('input', someFunction)
+
+
+
+
+
+/*function someFunction() {    
     document.getElementById('outp').textContent = document.querySelector('#inp').value;
     if (!document.querySelector('#inp').value.length) {
         document.getElementById('outp').textContent = "Пусто";
     }
-}
-let inputValue = document.querySelector('#inp')
-inputValue.addEventListener('input', someFunction)
+}*/
