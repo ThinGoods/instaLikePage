587122b2e3a9e2fd62f322c7d1b23e9a61513f8b
ThinGoods <vashchenko131@gmail.com>
2022-03-18 14:21:57 +0200

    some small chenges

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,5 @@
+// let outputValue = document.querySelector('#outp')
+// outputValue.innerHTML = '<img src="../images/redSquare.png" alt="не загрузилось"/>'
 function someFunction() {   
     let divOutput = document.getElementById('outp');
     let inputRow = document.querySelector('#inp')
@@ -18,6 +20,26 @@ inputValue.addEventListener('input', someFunction)
 
 
 
+
+
+
+
+let buttonOfCreationValue = document.querySelector(".buttonOfCreation");
+buttonOfCreationValue.addEventListener('click', createRedSquarefnct);
+
+function createRedSquarefnct() {
+    const outputValue = document.querySelector('#outpSquare');
+    let img = document.createElement('img');
+    img.src = 'redSquare.png';
+    console.log(img);
+    outputValue.append(img)
+}
+// const outputValue = document.querySelector('#outp');
+// let img = document.createElement('img');
+// img.src = 'redSquare.png';
+// outputValue.append(img)
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 /*function someFunction() {    
     document.getElementById('outp').textContent = document.querySelector('#inp').value;
